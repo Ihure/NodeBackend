326bdb5ea832a050d42599a13a0db79b37108c0f
ihure <ihure.chris@gmail.com>
2019-07-09 20:37:46 +0300

    configured docker

--- app/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-import routes from './v1';
-
-//register routes
-router.use('/', routes);
-
-module.exports = router;
--- app/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-import  Router from 'express';
-import  customerRoute from './customer.routes';
-
-const routes = Router();
-
-routes.use('/', customerRoute);
-
-module.exports = routes;
